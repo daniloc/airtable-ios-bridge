1f86bf8ed57f348a3e48496fd7067cf12df3c5ca
Danilo Campos <mail@danilocampos.com>
2018-08-17 14:25:28 -0400

    CRUD methods implemented

--- AirtableBridge.m
+++ AirtableBridge.m
@@ -60,9 +60,11 @@ NSString *defaultApiRoot = @"https://api.airtable.com/v0";
     return components.URL;
 }
 
-- (NSURLRequest *)authorizedRequestWithURL:(NSURL *)url {
+- (NSMutableURLRequest *)authorizedRequestWithURL:(NSURL *)url {
     NSMutableURLRequest *request = [NSURLRequest requestWithURL:url].mutableCopy;
-    request.allHTTPHeaderFields = @{ @"Authorization" : [@"Bearer " stringByAppendingString:self.apiKey] };
+    request.allHTTPHeaderFields = @{ @"Authorization" : [@"Bearer " stringByAppendingString:self.apiKey],
+                                     @"Content-type": @"application/json"
+                                     };
     
     return request;
 }
@@ -128,7 +130,7 @@ NSString *defaultApiRoot = @"https://api.airtable.com/v0";
                                           for (NSDictionary *dictionary in json[@"records"]) {
                                               [results setObject:dictionary[@"fields"] forKey:dictionary[@"id"]];
                                           }
-                                          handler(results, json[@"offset"], nil);
+                                          handler(results, json[@"offset"], parseError);
                                       }
                                   }];
     
@@ -146,12 +148,15 @@ NSString *defaultApiRoot = @"https://api.airtable.com/v0";
     joinedRecordIDs = [@"RECORD_ID()='" stringByAppendingString:joinedRecordIDs];
     NSString *filterFormula = [NSString stringWithFormat:@"OR(%@')", joinedRecordIDs];
     
-    return [self loadTable:tableName
-                  atOffset:nil
-             filterFormula:filterFormula
-                maxRecords:100
-                  viewName:viewName
-         completionHandler:handler];
+    NSURLSessionDataTask *task = [self loadTable:tableName
+                                  atOffset:nil
+                             filterFormula:filterFormula
+                                maxRecords:100
+                                  viewName:viewName
+                         completionHandler:handler];
+    
+    [task resume];
+    return task;
 }
 
 - (NSURLSessionDataTask *)loadRecordWithID:(NSString *)recordID
@@ -176,7 +181,105 @@ NSString *defaultApiRoot = @"https://api.airtable.com/v0";
                                                                                                options:0
                                                                                                  error:&parseError];
                                           
-                                          handler(json[@"fields"], nil);
+                                          handler(json[@"fields"], parseError);
+                                      }
+                                  }];
+    
+    [task resume];
+    return task;
+}
+
+- (NSURLSessionDataTask *)createRecord:(NSDictionary *)record
+                               inTable:(NSString *)tableName
+                     completionHandler:(void (^)(NSString *newRecordID, NSError *error))handler {
+    
+    NSURLSessionDataTask *task = [self updateRecordID:nil
+                                           withFields:record
+                                              inTable:tableName
+                                    completionHandler:handler];
+    return task;
+    
+    
+}
+
+- (NSURLSessionDataTask *)updateRecordID:(NSString *)recordID
+                              withFields:(NSDictionary *)fields
+                                 inTable:(NSString *)tableName
+                       completionHandler:(void (^)(NSString *recordID, NSError *error))handler {
+    
+    NSURL *url = [self.baseURL URLByAppendingPathComponent:tableName];
+    
+    if (recordID) {
+        url = [url URLByAppendingPathComponent:recordID];
+        //If recordID is nil, we create a new record. If not, we update it.
+    }
+    
+    NSMutableURLRequest *request = [self authorizedRequestWithURL:url];
+    
+    if (recordID){
+        request.HTTPMethod = @"PATCH";
+    } else {
+        request.HTTPMethod = @"POST";
+    }
+    
+    NSError *error;
+    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:@{@"fields": fields}
+                                                       options:NSJSONWritingSortedKeys
+                                                         error:&error];
+    
+    if (error) {
+        [NSException raise:@"Error encoding dictionary to JSON"
+                    format:@"Details: %@", error];
+    }
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:
+                                  ^(NSData *data, NSURLResponse *response, NSError *error) {
+                                      if (error) {
+                                          NSLog(@"Error %@", error);
+                                          handler(nil, error);
+                                      } else {
+                                          
+                                          NSError *parseError = nil;
+                                          NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data
+                                                                                               options:0
+                                                                                                 error:&parseError];
+                                          
+                                          handler(json[@"id"], parseError);
+                                      }
+                                  }];
+    
+    [task resume];
+    return task;
+    
+}
+
+- (NSURLSessionDataTask *)deleteRecord:(NSString *)recordID
+                               inTable:(NSString *)tableName
+                     completionHandler:(void(^)(BOOL deletedSuccessfully, NSError *error))handler {
+    
+    NSURL *url = [self.baseURL URLByAppendingPathComponent:tableName];
+    url = [url URLByAppendingPathComponent:recordID];
+    
+    NSMutableURLRequest *request = [self authorizedRequestWithURL:url];
+    request.HTTPMethod = @"DELETE";
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:
+                                  ^(NSData *data, NSURLResponse *response, NSError *error) {
+                                      if (error) {
+                                          NSLog(@"Error %@", error);
+                                          handler(nil, error);
+                                      } else {
+                                          
+                                          NSError *parseError = nil;
+                                          NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data
+                                                                                               options:0
+                                                                                                 error:&parseError];
+                                          
+                                          handler([json[@"deleted"] boolValue], parseError);
                                       }
                                   }];
     
