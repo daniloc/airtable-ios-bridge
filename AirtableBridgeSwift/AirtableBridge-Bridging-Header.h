8ceca663706656a390768d6dcef003f7621f9b68
Danilo Campos <weeb@mac.com>
2018-08-20 13:05:44 -0400

    Add Swift project with bridge header to Objective-C files

---
+++ AirtableBridgeSwift/AirtableBridge-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  AirtableBridge-Bridging-Header.h
+//  AirtableBridgeSwift
+//
+//  Created by Danilo Campos on 8/20/18.
+//  Copyright © 2018 Danilo Campos. All rights reserved.
+//
+
+#ifndef AirtableBridge_Bridging_Header_h
+#define AirtableBridge_Bridging_Header_h
+
+
+#import "AirtableBridge.h"
+#import "APIKey.h"
+
+
+#endif /* AirtableBridge_Bridging_Header_h */
